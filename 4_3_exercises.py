6c03bfbb07df1b8875b2a206f3cabc567832e281
vanessavela <vanessa@sirius.utp.edu.co>
2017-06-09 17:50:27 -0500

    added refactoring

--- 4_3_exercises.py
+++ 4_3_exercises.py
@@ -80,14 +80,39 @@ bob = Turtle()
 bob.delay = 0.01
 
 from math import pi
+"""
 def arc(t,r,angle):
     arc_length = (2 * pi * r * angle)/360
     n = int(arc_length / 3) + 1
     step_length = arc_length / n
     step_angle = float(angle) / n
+
     for i in range(n):
         fd(t, step_length)
         lt(t, step_angle)
+
 arc(bob,30,45)
 wait_for_user()
-        
+"""
+#refactoring
+def polyline(t,n,length, angle):
+    for i in range(n):
+        fd(t,length)
+        lt(t,angle)
+
+def polygon(t,n,length):
+    angle = 360.0/n
+    polyline(t, n, length, angle)
+
+def arc(t,r,angle):
+    arc_length = (2 * pi * r * angle)/360
+    n = int(arc_length / 3) + 1
+    step_length = arc_length / n
+    step_angle = float(angle) / n
+    polyline(t, n, step_length, step_angle)
+
+def circle(t,r):
+    arc(t,r,360)
+
+circle(bob,50)
+wait_for_user()
