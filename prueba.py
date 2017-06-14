a4fd32f107ef6c8bbf7488ed4b12742dbcd626f7
vanessavela <vanessa@sirius.utp.edu.co>
2017-06-14 17:23:42 -0500

    code final in turtle_pies

--- prueba.py
+++ prueba.py
@@ -3,30 +3,55 @@ world = TurtleWorld()
 bob = Turtle()
 bob.delay = 0.01
 
-from math import pi
+import math
 
-def polyline(t,n,length, angle):
-    """draw n line segments
+def isosceles(t, r, angle):
+    """Draws an isosceles triangle.
+
+    The turtle starts and ends at the peak, facing the middle of the base.
 
     t: Turtle
-    n: number of line segments
-    length: length of each segment
-    angle: degrees between segments
+    r: length of the equal legs
+    angle: peak angle in degrees
     """
+    y = r * math.sin(angle * math.pi / 180)
+
+    rt(t, angle)
+    fd(t, r)
+    lt(t, 90+angle)
+    fd(t,2*y)
+    lt(t, 90+angle)
+    fd(t, r)
+    lt(t, 180-angle)
+
+def polypie(t, n, r):
+    """Draws a pie divided into radial segments.
+
+    t: Turtle
+    n: number of segments
+    r: length of the radial spokes
+    """
+    angle = 360.0 / n
     for i in range(n):
-        fd(t,length)
-        lt(t,angle)
+        isosceles(t, r, angle/2)
+        lt(t, angle)
 
-def trian(t,length):
-    """draws a trian with n sides
+def draw_pie(t, n, r):
+    """Draws a pie, then moves into position to the right.
 
     t: Turtle
-    n: number of line segments
-    length: length of each segment
+    n: number of segments
+    r: length of the radial spokes
     """
-    n=3
-    angle = 360.0/n
-    polyline(t, n, length, angle)
-    
-trian(bob,50)
+    polypie(t, n, r)
+    pu(t)
+    fd(t, r*2 + 10)
+    pd(t)
+
+
+size = 40
+draw_pie(bob,5,size)
+draw_pie(bob,6,size)
+draw_pie(bob,7,size)
+
 wait_for_user()
